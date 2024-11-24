505f961dfe1c5410f3c50dbb18f2a10c7d1e93bf
gracetownland <speak2ayushsrihari@gmail.com>
2024-11-24 15:37:29 -0800

    added stubs

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,6 +4,8 @@ import background from './img/SiyaDwivedi.png'
 function LandingPage() {
   return (
     <div className="LandingPage" style={{backgroundImage: `url(${background})`}}>
+      </div>
+      <div>
       <header className="LandingPage-header">
         <h1>Welcome to the Landing Page</h1>
         <p>This is the landing page of your application.</p>
