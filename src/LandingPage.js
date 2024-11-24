93c88e5f48639b26b67b715e10ace28a1d80ca12
gracetownland <speak2ayushsrihari@gmail.com>
2024-11-24 15:34:56 -0800

    added stubs

---
+++ src/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css'; // Create this CSS file for specific styling
+import background from './img/SiyaDwivedi.png'
+function LandingPage() {
+  return (
+    <div className="LandingPage" style={{backgroundImage: `url(${background})`}}>
+      <header className="LandingPage-header">
+        <h1>Welcome to the Landing Page</h1>
+        <p>This is the landing page of your application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
