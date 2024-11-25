375acd54f7cc741942a1882c24524c0ec9bcb8fd
gracetownland <speak2ayushsrihari@gmail.com>
2024-11-24 17:06:42 -0800

    made first page

---
+++ src/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AboutMe.css'; // Create this CSS file for specific styling
+
+function AboutMe() {
+  return (
+    <div className="AboutMe">
+      <h2>Get to know</h2>
+      <>Siya</>
+      <p>This is just tingz</p>
+    </div>
+  );
+}
+
+export default AboutMe;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import LandingPage from './LandingPage';
-
+import AboutMe from './AboutMe'
 function App() {
   return (
     <div className="App">
       <LandingPage></LandingPage>
+      <AboutMe></AboutMe>
     </div>
   );
 }
