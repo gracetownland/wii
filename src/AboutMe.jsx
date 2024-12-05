2b162f3c7c859d9e58062f90ab755cb229e15ca4
gracetownland <speak2ayushsrihari@gmail.com>
2024-12-04 20:32:11 -0800

    added stubs for next couple of pages

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './AboutMe.css'; // Create this CSS file for specific styling
-
+import portfolio from './img/portfolio.jpg'
+import portfolio1 from './img/portfolio1.png'
 function AboutMe() {
   return (
     <div className="AboutMe">
-      <h2>Get to know</h2>
-      <>Siya</>
-      <p>This is just tingz</p>
+      <div className = "leftSide">
+      <h2 className = "heading">Get to Know </h2>
+      <h2 className="name">Siya</h2>
+      </div>
+      <div className = "rightSide">
+      <p>Siya is an UBC Alumnus and the owner of Tiny Mango Shop, where she sells handmade jewellery and crocheted items. She loves Kathak, Khalid, Coldplay and momos.</p>
+      </div>
     </div>
   );
 }
---
+++ src/ContactPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ContactPage.css'; // Create this CSS file for specific styling
+import background from './img/SiyaDwivedi.png'
+
+function ContactPage() {
+  return (
+    <div className="AboutMe">
+    <div className='backgroundImage' style={{backgroundImage: `url(${background})`}}>
+
+
+      <p>TODO.</p>
+      </div>
+    </div>
+  );
+}
+
+export default ContactPage;
