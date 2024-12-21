4ed615cbd9a6ae2e500340ed8b447bb34a24aa96
gracetownland <speak2ayushsrihari@gmail.com>
2024-12-21 12:51:22 -0800

    dirty navbar added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 import './NavBar.css'
 function NavBar(){
     return (
-        <>hello MAN</>
+<nav className="NavBar">
+                        <a href="/#work" className="nava">
+                            Work
+                        </a>
+                        <a href="/#projects" className="nava">
+                            Projects
+                        </a>
+                        <a href="/#research" className="nava">
+                            Research
+                        </a>
+                        <a href="/">
+                            <>aaa</>
+                        </a>
+                        <a href="/#about" className="nava">
+                            About
+                        </a>
+                        <a href="/#contact" className="nava">
+                            Contact
+                        </a>
+                        <a href="/Icons" className="nava">
+                            Icons
+                        </a>
+                    </nav>
     )
 }
 export default NavBar
