e5fb3b0bb84dcb0d462f6ed9c8281d5168d5ced7
gracetownland <speak2ayushsrihari@gmail.com>
2024-12-21 12:39:09 -0800

    added all images

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import LandingPage from './LandingPage';
 import AboutMe from './AboutMe'
 import ContactPage from './ContactPage';
+import QuotePage  from './QuotePage';
 function App() {
   return (
     <div className="App">
       <LandingPage></LandingPage>
       <AboutMe></AboutMe>
+    <QuotePage></QuotePage>
       <ContactPage></ContactPage>
     </div>
   );
--- src/QuotePage.jsx
+++ src/QuotePage.jsx
@@ -1,1 +1,17 @@
-//TODO
+import React from 'react';
+import './ContactPage.css'; // Create this CSS file for specific styling
+import background from './img/Quote.jpg'
+
+function ContactPage() {
+  return (
+    <div className="AboutMe">
+    <div className='backgroundImage' style={{backgroundImage: `url(${background})`}}>
+
+
+      <p>TODO.</p>
+      </div>
+    </div>
+  );
+}
+
+export default ContactPage;
