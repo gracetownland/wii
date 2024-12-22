bf4e10bf33a01a1d9e6579b6a5d80ef9a402f65e
gracetownland <speak2ayushsrihari@gmail.com>
2024-12-22 11:54:31 -0800

    added images and description for aboutMe page

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './AboutMe.css'; // Create this CSS file for specific styling
+import aboutMe from './img/aboutMe.JPG'
 import portfolio from './img/portfolio.jpg'
 import portfolio1 from './img/portfolio1.png'
 function AboutMe() {
   return (
     <div id="AboutMe" className="AboutMe">
       <div className = "leftSide">
-      <h2 className = "heading">Get to Know </h2>
-      <h2 className="name">Siya</h2>
+        <div className='text'>
+      <h2 className = "heading">Get to know</h2>
+      <h2 className="Myname">Siya</h2>
+        </div>
+ <div className='leftImage'>
+      <img src={aboutMe} alt="pretty siya smiling" />
+    </div>
       </div>
       <div className = "rightSide">
-      <p>Siya is an UBC Alumnus and the owner of Tiny Mango Shop, where she sells handmade jewellery and crocheted items. She loves Kathak, Khalid, Coldplay and momos.</p>
+        <div>
+      <img src={portfolio} alt="pretty siya smiling" />
+    </div>
+      <p className='bodyText'>Siya is an UBC Alumnus and the owner of Tiny Mango Shop, where she sells handmade jewellery and crocheted items. She loves Kathak, Khalid, Coldplay and momos.</p>
       </div>
     </div>
   );
