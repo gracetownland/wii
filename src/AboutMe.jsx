19b7812b8c40a5d97d15078ea48d77f1eccca901
gracetownland <speak2ayushsrihari@gmail.com>
2024-12-26 12:53:31 -0800

    email form works. change routing

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -8,18 +8,21 @@ function AboutMe() {
     <div id="AboutMe" className="AboutMe">
       <div className = "leftSide">
         <div className='text'>
-      <h2 className = "heading">Get to know</h2>
+      <h2 className = "heading">Get to</h2>
+      <span>
+      <h2 className = "heading">know</h2>
       <h2 className="Myname">Siya</h2>
+      </span>
         </div>
  <div className='leftImage'>
       <img src={aboutMe} alt="pretty siya smiling" />
     </div>
       </div>
       <div className = "rightSide">
-        <div>
+        <div className=''>
       <img src={portfolio} alt="pretty siya smiling" />
-    </div>
       <p className='bodyText'>Siya is an UBC Alumnus and the owner of Tiny Mango Shop, where she sells handmade jewellery and crocheted items. She loves Kathak, Khalid, Coldplay and momos.</p>
+    </div>
       </div>
     </div>
   );
--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import emailjs from 'emailjs-com';
 import './ContactPage.css'; // Create this CSS file for specific styling
 import background from './img/ContactPage.jpg'
 
 function ContactPage() {
-  return (
+  //need to use state variables to store values
+const [name,setName] = useState('');
+const [email, setEmail] = useState('');
+const [message,setMessage]=useState('');
 
-    <div id='ReachOut' className='backgroundImage' style={{backgroundImage: `url(${background})`}}>
+//Form submission
+const handleSubmti= (e)=>{
+  e.preventDefault();
+const templateParams = { 
+  from_name: name, 
+  from_email: email,
+   message: message,
+   }; 
+   emailjs.send('service_5ue8zyb', 'template_e1zdujn', templateParams, 'WlyfmaU-3df4UeAfb') .then((response) => { console.log('SUCCESS!', response.status, response.text); }, (err) => { console.error('FAILED...', err); }); }
 
-
-      
+return (
+  <>
+  <div id='ReachOut' className='backgroundImage' style={{backgroundImage: `url(${background})`}}>
+  <div className='titleCard'>
+GET IN TOUCH  
       </div>
+      <form onSubmit={handleSubmti}>
+        <div><label htmlFor="name">Name:</label> 
+          <input type='text' id='name' value={name} onChange={(e)=> setName(e.target.value)} required>
+          </input>
+        </div>
+        <div> <label htmlFor="email">Email:</label> <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required /> </div> <div> <label htmlFor="message">Message:</label> <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} required /> </div> <button type="submit">Submit</button>
+      </form>   
+    </div>
+  </>
 
   );
 }
