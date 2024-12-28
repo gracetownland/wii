e8e6202e3fa74e7daa5c7a7edf738a8ad0c23d0f
gracetownland <speak2ayushsrihari@gmail.com>
2024-12-27 23:57:38 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -3,34 +3,12 @@ import LandingPage from './LandingPage';
 import AboutMe from './AboutMe';
 import ContactPage from './ContactPage';
 import QuotePage from './QuotePage';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { useEffect } from 'react';
-
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 function App() {
-  useEffect(() => {
-   const handleScroll = (event) => {
-  event.preventDefault();
-  const direction = event.deltaY > 0 ? 1 : -1;
-  const scrollAmount = (window.innerHeight * direction);
-  gsap.to(window, { scrollTo: { y: window.scrollY + scrollAmount, autoKill: false }, duration: 1.5, ease: "power2.inOut" });
-};
-
-    window.addEventListener('wheel', handleScroll);
-
-    return () => {
-      window.removeEventListener('wheel', handleScroll);
-    };
-  }, []);
-
   return (
     <div className="App">
       <LandingPage />
       <AboutMe />
-      <QuotePage />
       <ContactPage />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Root() {
+  useEffect(() => {
+    const handleScroll = (event) => {
+      event.preventDefault();
+      const direction = event.deltaY > 0 ? 1 : -1;
+      const scrollAmount = window.innerHeight * direction; // Scroll by the height of the viewport
+      gsap.to(window, { scrollTo: { y: window.scrollY + scrollAmount, autoKill: false }, duration: 2.0, ease: "power1.inOut" });
+    };
+
+window.addEventListener("wheel", handleScroll, { passive: false });
+
+    return () => {
+window.removeEventListener('wheel', handleScroll, { passive: false });    };
+  }, []);
+
+  return <App />;
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
