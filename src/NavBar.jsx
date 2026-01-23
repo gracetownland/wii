da84528a5962ac9cef4992cb6b11e4c53d4a1f0f
gracetownland <speak2ayushsrihari@gmail.com>
2026-01-23 11:57:23 -0800

    fixed link error

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,30 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './NavBar.css';
 
-
 function NavBar() {
-  useEffect(() => {
-    const links = document.querySelectorAll('.nava');
-    links.forEach(link => {
-      link.addEventListener('click', (event) => {
-        event.preventDefault();
-        // Navigation handled by default link behavior
-      });
-    });
-
-    return () => {
-      links.forEach(link => {
-        link.removeEventListener('click', () => { });
-      });
-    };
-  }, []);
-
   return (
     <nav className="NavBar">
-      <a href="https://www.instagram.com/tinymangoshop/" className="nava">
+      <a href="https://www.instagram.com/tinymangoshop/" className="nava" target="_blank" rel="noopener noreferrer">
         Tiny Mango
       </a>
-      <a href="https://moonlitmirage.home.blog/" className="nava">
+      <a href="https://moonlitmirage.home.blog/" className="nava" target="_blank" rel="noopener noreferrer">
         Moonlit Mirage
       </a>
       <a href="#AboutMe" className="nava">
